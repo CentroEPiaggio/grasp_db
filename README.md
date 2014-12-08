fa004b1f3a8d60a140b8264794048a6a9346dbee
Alessandro <ale.settimi@gmail.com>
2014-12-08 22:42:25 +0100

    First Commit, just a catkin package :P

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dual Manipulation Grasp Database #
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<std::endl;
+    std::cout<<"I'm the dual manipulation grasping database!!! . . . implement me!"<<std::endl;
+    std::cout<<std::endl;
+}
