9dcdfac7c7f21ac22d77c339de9472be0106a6c8
MirkoFerrati <mirko.ferrati@gmail.com>
2014-12-10 00:49:35 +0100

    first sqlite3 support included

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 
+#include <sqlite3.h> 
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+    int i;
+    for(i=0; i<argc; i++)
+    {
+        std::cout<<azColName[i]<<" = "<<(argv[i] ? argv[i] : "NULL")<<std::endl;  
+    }
+    return 0;
+}
+
 int main()
 {
-    std::cout<<std::endl;
-    std::cout<<"I'm the dual manipulation grasping database!!! . . . implement me!"<<std::endl;
-    std::cout<<std::endl;
+    sqlite3 *db;
+    int rc;
+    /* Open database */
+    rc = sqlite3_open("test.db", &db);
+    if( rc ){
+        std::cout<< "Can't open database" << sqlite3_errmsg(db)<<std::endl;
+        return 0;
+    }else{
+        std::cout<< "Opened database successfully" <<std::endl;
+    }
+
+    const char *sql="SELECT name FROM sqlite_master WHERE type='table';";
+    char* errMsg;
+    rc= sqlite3_exec(db,sql,callback,0,&errMsg);
+    if( rc!=SQLITE_OK )
+    {
+        std::cout<< "SQL error: "<<errMsg<<std::endl;
+        sqlite3_free(errMsg);
+    }
+    sqlite3_close(db);
+
+    return 0;
 }
